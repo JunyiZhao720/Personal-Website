7e04b4ec84a816498cf135e5737807d935ba6c30
zjylh <zjylh720@gmail.com>
2020-01-22 00:24:42 -0500

    done basic page set up

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,21 +4,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header className="masthead mb-auto">
+        <div className="inner">
+          <h3 className="masthead-brand">Cover</h3>
+          <nav className="nav nav-masthead justify-content-center">
+            <a className="nav-link active" href="#">Home</a>
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
       </header>
+
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">Cover your page.</h1>
+        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p className="lead">
+          <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+      </footer>
     </div>
   );
 }
