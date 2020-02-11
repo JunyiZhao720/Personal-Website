4c3298dd30a847e773794fa7903f11b19966482f
zjylh <zjylh720@gmail.com>
2020-02-11 12:33:14 -0500

    Done routing and nav value change

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Home from './Components/home'
+import Home from './Components/home';
+import NavBar from './Components/navBar';
+import Profile from './Components/profile';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// const PageContainer = styled.div`
+//   position: relative;
+//   width: 100vw;
+//   height: 100vh;
+//   background-color: #e3f2fd;
+//   font-family: "Open Sans", sans-serif;
+// `;
 
 function App() {
+  // return(
+  //   <Router>
+  //     <React.Fragment>    
+  //         <header className="masthead mb-auto">
+  //           <div className="inner">
+  //             <h3 className="masthead-brand">University of Waterloo</h3>
+  //             <NavBar />
+  //           </div>
+  //         </header>
+          
+  //       <Switch>
+  //         <Route path="/" exact component={Home}/>
+  //         <Route path="/profile" component={Profile}/>
+  //       </Switch>
+  
+  //     </React.Fragment>
+  //   </Router>
+  // );
+
   return (
-    <Home />
+    <Router>
+      <React.Fragment>
+        <header className="masthead mb-auto">
+          <div className="inner">
+            <h3 className="masthead-brand">University of Waterloo</h3>
+            <NavBar />
+          </div>
+        </header>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/profile" component={Profile}/>
+          </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 
--- src/main/frontend/src/Components/navBar.jsx
+++ src/main/frontend/src/Components/navBar.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 class NavBar extends Component {
+    state ={
+        selection: 3,
+
+        options: [
+            {id: 0, value: "Home", link: "/"},
+            {id: 1, value: "Profile", link: "/profile"},
+        ]
+    }
+
+    formatSelection(id){
+        const{ selection } = this.state;
+        let format = "nav-link";
+        if (selection === id) return format + " active";
+
+        return format;
+    }
+
+    handleSelect = (id) => {
+        this.setState({selection: id});
+    }
+
     render() { 
         return (  
             <nav className="nav nav-masthead justify-content-center">
-                <a className="nav-link active" href="#">Home</a>
-                <a className="nav-link" href="#">Blogs</a>
-                {/* <a className="nav-link" href="#">Contact</a> */}
-            </nav>
-        );
+                {this.state.options.map(opt => 
+                    <Link 
+                    className={this.formatSelection(opt.id)} 
+                    to={opt.link} 
+                    key={opt.id} 
+                    onClick={()=>this.handleSelect(opt.id)} >
+
+                        {opt.value}
+                    
+                    </Link>
+                )}
+            </nav>);
     }
 }
  
