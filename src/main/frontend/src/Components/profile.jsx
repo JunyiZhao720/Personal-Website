3c071c894e8de8f1bf378da7c36de697a85c98c4
zjylh <zjylh720@gmail.com>
2020-02-11 12:48:47 -0500

    Add basic image switch

--- src/main/frontend/src/Components/profile.jsx
+++ src/main/frontend/src/Components/profile.jsx
@@ -14,7 +14,7 @@ class Profile extends Component {
             <CustomizedTransition timeout={2000} classNames={"fade"} >
 
               <main role="main" className="row inner cover">
-                
+
                 <div className="col-sm">
                   <h1 className="cover-heading">{this.state.main_title}</h1>
                   <p className="lead">{this.state.main_description}</p>
@@ -24,7 +24,24 @@ class Profile extends Component {
                 </div>     
 
                 <div className="col-sm">
-                  
+                  <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div className="carousel-inner">
+                      <div className="carousel-item active">
+                        <img className="d-block w-100" src="https://i.imgur.com/J8oZ6J3.jpg" alt="First slide" style={{width: "200px", height: "300px" }}/>
+                      </div>
+                      <div className="carousel-item">
+                        <img className="d-block w-100" src="https://channel-korea.com/wp-content/uploads/2018/07/taeyeon.jpg" alt="Second slide"/>
+                      </div>
+                      <div className="carousel-item">
+                        <img className="d-block w-100" src="https://vignette.wikia.nocookie.net/kpop/images/c/cf/Taeyeon_%28Purpose%29_promo_photo_1.png/revision/latest?cb=20200110034639" alt="Third slide"/>
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
               </main>
